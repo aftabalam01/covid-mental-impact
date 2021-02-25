db5931ea84fe9cf65b41d46553752f3a42041a11
Aftab Alam <aftab.alam@aftabalam-ltm.internal.salesforce.com>
2021-02-25 14:38:29 -0800

    working with geo filter

--- search_full_archive.py
+++ search_full_archive.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime,timedelta 
 from searchtweets import collect_results
 from searchtweets import ResultStream, load_credentials, gen_request_parameters
 
@@ -28,42 +28,64 @@ def fetch_results(query_rule,result_stream_args,max_results=100):
 
 if __name__=='__main__':
     START_OVER = False
-    search_args = credential_args(filename="./credential.yaml",yaml_key="search_tweets_premium")
-    #print(search_args)
-    rule = search_rules("COVID",filters="lang:en place:Seattle",fromdate="2019-06-09",todate="2020-06-10")
-    #print(rule)
-    tweets = fetch_results(rule,search_args,10)
+    MAX_TWEETS = 1000
     if START_OVER:
         with open('tweets_response.csv','w') as tweet_f:
-            tweet_f.write('tweet_id|created_at|author_id|language|inreply_to|possibly_sensitive|retweet_count|reply_count|like_count|quote_count')
+            tweet_f.write('tweet_id|created_at|author_id|language|inreply_to|possibly_sensitive|retweet_count|reply_count|like_count|quote_count|search_type|search_city')
         with open('tweets_users.csv','w') as user_f:
             f.write('author_id|id|location|verified')
         with open('tweets_place.csv','w') as place_f:
             user_f.write('place_id|place_type|name|country_code')
     
-    for tweet in tweets:
-        if 'users' in tweet.keys():
-            for user in tweet['users']:
-                with open('tweets_place.csv','a') as place_f:
-                    user_f.write(f"{user.get('author_id')}|{user.get('id')}|{user.get('location')}|{user.get('verified')}\n")
-            continue
+    search_args = credential_args(filename="./credential.yaml",yaml_key="search_tweets_premium")
+    #print(search_args)
+    rule = search_rules("COVID",filters="lang:en place:Seattle",fromdate="2019-06-09",todate="2020-06-10")
+    #print(rule)
+    cities = [['New York',[40.730610,-73.935242,20]],
+            ['Seattle',[47.608013,-122.335167,20]]]#'London','Hawaii','Miami','Mumbai','Delhi','Seoul','Singapore','Tokyo','Rome','Auckland','Syndey','Cape Town']
+    covid_keywords = ['Coronavirus']
+    mental_health_keywords= ['mental health']
+    covid_search_term = " OR ".join(covid_keywords)
+    mental_search_term = " OR ".join(mental_health_keywords)
+    keywords = {'covid_search':covid_search_term,'mental_search': mental_search_term}
+    START_DATE = "2019-12-01"
+    default_place={'place_id':0}
+    for i in range(0,3,1):
+        datetime_object = datetime.strptime(START_DATE, '%Y-%m-%d')
+        query_start_date = datetime_object+ timedelta(i)
+        query_end_date = datetime_object+ timedelta(i+1)
+        print(f"Quering for start date {query_start_date} and end date {query_end_date}")
+        for query, value in keywords.items():
+            for city, radius in cities:
+                #point_radius:[longitude latitude radius]
+                rule = search_rules(value,filters=f"lang:en -is:retweet has:geo (place:{city} OR point_radius:[{radius[1]} {radius[0]} {radius[2]}mi])"
+                                    ,fromdate=query_start_date.strftime('%Y-%m-%d')
+                                    ,todate=query_end_date.strftime('%Y-%m-%d'))
+                print(f"Query Rule {rule}")
+                tweets = fetch_results(rule,search_args,MAX_TWEETS)
+                for tweet in tweets:
+                    if 'users' in tweet.keys():
+                        for user in tweet['users']:
+                            with open('tweets_place.csv','a') as place_f:
+                                user_f.write(f"{user.get('author_id')}|{user.get('id')}|{user.get('location')}|{user.get('verified')}\n")
+                        continue
 
-        if 'places' in tweet.keys():
-            for place in tweet['places']:
-                with open('tweets_users.csv','a') as user_f:
-                    place_f.write(f"{place.get('place_id')}|{place.get('place_type')}|{place.get('name')}|{place.get('country_code')}\n")
-            continue
-        text = tweet['text'].replace('\n', ' ').replace('\r', '')
-        with open('tweets_response.csv','a') as tweet_f:
-            tweet_f.write(f"{tweet['id']}|{tweet['created_at']}|{tweet['author_id']}| \
-            {text}|{tweet['lang']}|{tweet['geo']['place_id']}|\
-            {tweet.get('in_reply_to_user_id')}| \
-            {tweet['possibly_sensitive']}| \
-            {tweet['public_metrics']['retweet_count']}| \
-            {tweet['public_metrics']['reply_count']}| \
-            {tweet['public_metrics']['like_count']}| \
-            {tweet['public_metrics']['quote_count']}\n")
-        break
+                    if 'places' in tweet.keys():
+                        for place in tweet['places']:
+                            with open('tweets_users.csv','a') as user_f:
+                                place_f.write(f"{place.get('place_id')}|{place.get('place_type')}|{place.get('name')}|{place.get('country_code')}\n")
+                        continue
+                    if tweet.get('text'):
+                        text = tweet.get('text').replace('\n', ' ').replace('\r', '')
+                        with open('tweets_response.csv','a') as tweet_f:
+                            tweet_f.write(f"{tweet.get('id')}|{tweet.get('created_at')}|{tweet.get('author_id')}| \
+                            {text}|{tweet.get('lang')}|{tweet.get('geo',default_place)['place_id']}|\
+                            {tweet.get('in_reply_to_user_id')}| \
+                            {tweet.get('possibly_sensitive')}| \
+                            {tweet.get('public_metrics',{'retweet_count':0})['retweet_count']}| \
+                            {tweet.get('public_metrics',{'reply_count':0})['reply_count']}| \
+                            {tweet.get('public_metrics',{'reply_count':0})['like_count']}| \
+                            {tweet['public_metrics']['quote_count']}|{query}|{city}\n")
 
 
 #sample tweets response 
